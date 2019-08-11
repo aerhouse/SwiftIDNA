37cd40aac5e49b33521a581985234cf3f6275bc9
aerhouse <5475871+aerhouse@users.noreply.github.com>
2019-08-11 17:08:35 -0500

    Code point mapping tests.

--- Tests/SwiftIDNATests/SwiftIDNATests.swift
+++ Tests/SwiftIDNATests/SwiftIDNATests.swift
@@ -2,14 +2,35 @@ import XCTest
 @testable import SwiftIDNA
 
 final class SwiftIDNATests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftIDNA().text, "Hello, World!")
+    func testMap1() {
+        let input = "\u{00DF}"
+        let expected = "ss"
+        let computed = input.idnaMap()
+        XCTAssertEqual(expected, computed)
+    }
+    
+    func testMap2() {
+        let input = "\u{0390}"
+        let expected = "\u{03B9}\u{0308}\u{0301}"
+        let computed = input.idnaMap()
+        XCTAssertEqual(expected, computed)
+    }
+    
+    func testMap3() {
+        let input = "a\u{037A}b"
+        let expected = "a\u{0020}\u{03B9}b"
+        let computed = input.idnaMap()
+        XCTAssertEqual(expected, computed)
+    }
+    
+    func testMap4() {
+        let input = "a\u{180B}b"
+        let expected = "ab"
+        let computed = input.idnaMap()
+        XCTAssertEqual(expected, computed)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testMap1", testMap1),
     ]
 }
